b0793f2352258f0ae7d7863edc63f3498660b168
Robert Egeland <robegeland@yahoo.com>
2022-02-02 17:19:14 -0600

    commit tree ab5872dc67e73ec5cb5094b2c003caa30d892873

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    let firstLetter = string.charAt(0)
+    let capitol = firstLetter.toUpperCase()
+    return string.replace(firstLetter, capitol)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'" "]+/g, "")
+  }
+
+  static titleize(string) {
+    let split = string.split(' ')
+
+    let newArr = split.map(word => {
+      if(word !== 'the' && word !== 'a' && word !== 'an' && word !== 'but' && word !== 'of' && word !== 'and' && word !== 'for' && word !== 'at' && word !== 'by' && word !== 'from'){
+        let firstLetter = word.charAt(0)
+        let capitol = firstLetter.toUpperCase()
+        return word.replace(firstLetter, capitol)
+      }else{
+        return word
+      }
+    })
+    let phrase = newArr.join(' ')
+    let fLetter = phrase.charAt(0)
+    let capitol = fLetter.toUpperCase()
+    return phrase.replace(fLetter, capitol)
+  }
 }
